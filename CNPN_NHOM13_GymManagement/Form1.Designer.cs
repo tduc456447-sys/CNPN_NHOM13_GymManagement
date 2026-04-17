e376ef29202faf1ae183a074be92ca79d6502a41
Phuc <nguyenphuc18042005@gmail.com>
2026-04-17 09:13:23 +0700

    Đăng Nhập

--- CNPN_NHOM13_GymManagement/Form1.Designer.cs
+++ CNPN_NHOM13_GymManagement/Form1.Designer.cs
@@ -28,38 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.chkRemember = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
-            // button1
+            // txtUsername
             // 
-            this.button1.Location = new System.Drawing.Point(546, 196);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.txtUsername.Location = new System.Drawing.Point(352, 88);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 22);
+            this.txtUsername.TabIndex = 0;
             // 
-            // checkBox1
+            // txtPassword
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(496, 314);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(95, 20);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.txtPassword.Location = new System.Drawing.Point(352, 134);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 22);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tên Đăng Nhập";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(186, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mật Khẩu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(302, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Đăng Nhập Hệ Thống";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(238, 200);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(95, 23);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Đăng Nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(365, 200);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 6;
+            this.Exit.Text = "Thoát";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // chkRemember
+            // 
+            this.chkRemember.AutoSize = true;
+            this.chkRemember.Location = new System.Drawing.Point(365, 174);
+            this.chkRemember.Name = "chkRemember";
+            this.chkRemember.Size = new System.Drawing.Size(131, 20);
+            this.chkRemember.TabIndex = 7;
+            this.chkRemember.Text = "Ghi nhớ mật khẩu";
+            this.chkRemember.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chkRemember);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Form Đăng Nhập";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,8 +131,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.CheckBox chkRemember;
     }
 }
 
--- CNPN_NHOM13_GymManagement/Form1.cs
+++ CNPN_NHOM13_GymManagement/Form1.cs
@@ -7,14 +7,115 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
+using System.Drawing.Drawing2D;
 namespace CNPN_NHOM13_GymManagement
 {
     public partial class Form1 : Form
     {
+        // 🔗 Connection string (sửa nếu cần)
+        string connectionString =
+            "Data Source=.\\SQLEXPRESS;Initial Catalog=GymDB;Integrated Security=True";
+
         public Form1()
         {
             InitializeComponent();
+            this.BackColor = Color.FromArgb(30, 30, 30); // nền tối
+            this.ForeColor = Color.White;
+
+            // TextBox
+            txtUsername.BackColor = Color.FromArgb(50, 50, 50);
+            txtUsername.ForeColor = Color.White;
+            txtUsername.BorderStyle = BorderStyle.FixedSingle;
+
+            txtPassword.BackColor = Color.FromArgb(50, 50, 50);
+            txtPassword.ForeColor = Color.White;
+            txtPassword.BorderStyle = BorderStyle.FixedSingle;
+
+            // Button Login
+            btnLogin.BackColor = Color.FromArgb(0, 122, 204); // xanh đẹp
+            btnLogin.ForeColor = Color.White;
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.FlatAppearance.BorderSize = 0;
+
+            // Button Exit
+            Exit.BackColor = Color.FromArgb(200, 50, 50); // đỏ nhẹ
+            Exit.ForeColor = Color.White;
+            Exit.FlatStyle = FlatStyle.Flat;
+            Exit.FlatAppearance.BorderSize = 0;
+
+            // Checkbox
+            //chkRemember.ForeColor = Color.White;
+
+            // Ẩn mật khẩu
+            txtPassword.UseSystemPasswordChar = true;
+
+            // Nhấn Enter = login
+            this.AcceptButton = btnLogin;
+            //
+            txtUsername.Text = Properties.Settings.Default.Username;
+            txtPassword.Text = Properties.Settings.Default.Password;
+
+
+        }
+
+        // 🔐 Nút đăng nhập
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            // validate
+            if (username == "" || password == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ!");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    string query = "SELECT * FROM Users WHERE Username=@u AND Password=@p";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@u", username);
+                    cmd.Parameters.AddWithValue("@p", password);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        MessageBox.Show("Đăng nhập thành công!");
+
+                        // 👉 mở form chính
+                        MainForm main = new MainForm();
+                        main.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sai tài khoản hoặc mật khẩu!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi kết nối DB: " + ex.Message);
+                }
+            }
+
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
+        
+    
+
+    
+
+
